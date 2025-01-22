292164d9eb8272adcad8356aefebff134e59a70e
FarahHend <farahhend8gmail.com>
2025-01-22 22:53:13 +0100

    vf

---
+++ src/main/java/com/hend/KeycloakPostgreSQL/KeycloakPostgreSqlApplication.java
@@ -0,0 +1,13 @@
+package com.hend.KeycloakPostgreSQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakPostgreSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakPostgreSqlApplication.class, args);
+	}
+
+}
